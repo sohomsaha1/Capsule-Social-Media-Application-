e81f1e3b6e7805706c2b561e83dbe6035facc2bb
Sohom Saha <57738616+sohomsaha1@users.noreply.github.com>
2021-02-20 18:45:22 -0500

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,7 +5,7 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
+export const getPosts = async (req, res) => {  // since our posts are asynchronous request we must specify
     try {
         const postMessages = await PostMessage.find();
                 
