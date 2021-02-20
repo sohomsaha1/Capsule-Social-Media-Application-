b98f9ba323c54c4208f07e5de4683c6829e99cec
Sohom Saha <57738616+sohomsaha1@users.noreply.github.com>
2021-02-20 18:48:27 -0500

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+
+
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+
 import FileBase from 'react-file-base64';
 
 import useStyles from './styles';
@@ -8,6 +11,7 @@ import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
