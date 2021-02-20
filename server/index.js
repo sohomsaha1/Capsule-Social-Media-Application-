a71cffb3f53041236434af9607250c244ab7ffc6
Sohom Saha <57738616+sohomsaha1@users.noreply.github.com>
2021-02-20 18:49:55 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://sohom1357:5379u948@cluster0.vqcn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://sohom1357:CapsulePassword123@cluster0.vqcn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
